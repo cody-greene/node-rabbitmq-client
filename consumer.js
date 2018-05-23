c58bed09849c09d748e5592f2c0e8bc2f7f71c77
Cody Greene <cody-greene@users.noreply.github.com>
2018-05-23 12:16:20 -0700

    WIP

--- consumer.js
+++ consumer.js
@@ -1,5 +1,8 @@
+'use strict'
 const Connection = require('./lib/connection')
 const AMQP_URL = 'amqp://admin:admin@127.0.0.1:5002?heartbeat=20'
+
+/* eslint-disable no-console */
 
 function promisify(fn) {
   return (...args) => new Promise((resolve, reject) => fn(...args, (err, val) => {
@@ -7,32 +10,16 @@ function promisify(fn) {
     else resolve(val)
   }))
 }
-function promisifyPick(obj, methods) {
-  for (let method of methods) {
-    obj[method + 'Async'] = promisify(obj[method].bind(obj))
-  }
-}
 
-/**
- * Public interface:
- * new Connection(props)
- * new Connection(connectionString)
- * Connection#close(done)
- * Connection#acquire(done) => channel
- * Channel#release()
- * Channel#[classMethod](params, done) => responseParams
- * Channel#on('class.method', fn) => params
- * Channel#off('class.method'[, fn])
- */
-let amqp = new Connection(AMQP_URL)
 
-/* eslint-disable no-console */
+const amqp = new Connection(AMQP_URL)
 
 const acquire = promisify(amqp.pool.acquire.bind(amqp.pool))
 
 acquire()
   .then(ch => {
     console.log('channel ready')
+    // ch.basicQos
     ch.basicConsume({queue: 'foo', noAck: false}, (envelope) => {
       console.log(envelope)
       console.log(envelope.body.toString())
